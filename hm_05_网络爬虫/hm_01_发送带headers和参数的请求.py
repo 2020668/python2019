823903f755f034b546d7feebbbeca2dfcea45888
“2020668” <keen2020@outlook.com>
2019-06-23 11:55:03 +0800

    版本 0.0.1新增代理爬虫

--- hm_05_网络爬虫/hm_01_发送带headers和参数的请求.py
+++ hm_05_网络爬虫/hm_01_发送带headers和参数的请求.py
@@ -6,9 +6,9 @@ headers = {
     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) "
     "AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.110 Safari/537.36"}
 
-params = {"wd": "传智播客"}
+params = {"wd": "python"}
 url_temp = "https://www.baidu.com"
 response = requests.get(url_temp, headers=headers, params=params)
 print(response.status_code)
-# print(response.request.url)
+print(response.request.url)
 print(response.content.decode())
